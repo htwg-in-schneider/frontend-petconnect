7db931a8d0c52f51e79ca59f852da4ad1dfc677a
Bianca König <bi451koe@htwg-konstanz.de>
2026-05-16 18:47:03 +0200

    Iteration 8a

--- src/views/AusschreibungenView.vue
+++ src/views/AusschreibungenView.vue
@@ -1,8 +1,32 @@
 <script setup>
-import { ausschreibungen } from '@/data.js'
+//import { ausschreibungen } from '@/data.js'
+
+// neu: iteration8a
+import { ref, onMounted } from 'vue'
+
 import Navbar from '@/components/Navbar.vue'
 import Footer from '@/components/Footer.vue'
 import AusschreibungCard from '@/components/AusschreibungCard.vue'
+
+const ausschreibungen = ref([])
+
+const url = 'https://dummyjson.com/products'
+
+onMounted(async () => {
+  const response = await fetch(url)
+  const data = await response.json()
+  
+
+  ausschreibungen.value = data.products.map(product => ({
+    id: product.id,
+    name: product.title,
+    age: 2,
+    city: "Konstanz",
+    date: "01.08.2026",
+    type: product.id % 2 === 0 ? "Bezahlung" : "Tausch",
+    imageUrl: product.thumbnail
+  }))
+})
 </script>
 
 <template>
