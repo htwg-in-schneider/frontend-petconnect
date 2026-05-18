f4a9c129abd81616d78f80089b72541178929139
Bianca König <bi451koe@htwg-konstanz.de>
2026-05-18 09:36:03 +0200

    Blatt 4 b/e zwischenstand

--- src/views/AusschreibungenView.vue
+++ src/views/AusschreibungenView.vue
@@ -1,7 +1,7 @@
 <script setup>
 
 
-import { ref, onMounted } from 'vue'
+import { ref, onMounted, computed } from 'vue'
 
 import Navbar from '@/components/Navbar.vue'
 import Footer from '@/components/Footer.vue'
@@ -10,9 +10,35 @@ const url = 'http://localhost:8081/api/ausschreibungen';
 import Button from '@/components/Button.vue'
 
 const ausschreibungen = ref([])
+const search = ref('')
+const filterAnimal = ref('')
+const filterCity = ref('')
+const filterCompensation = ref('')
 
 onMounted(async () => fetchAusschreibungen())
   
+  const filteredAusschreibungen = computed(() => {
+
+  return ausschreibungen.value.filter(a => {
+
+    const matchesSearch =
+      a.petName.toLowerCase().includes(search.value.toLowerCase()) ||
+      a.city.toLowerCase().includes(search.value.toLowerCase())
+
+    const matchesAnimal =
+      filterAnimal.value === '' || a.animalType === filterAnimal.value
+
+    const matchesCity =
+      filterCity.value === '' || a.city.toLowerCase().includes(filterCity.value.toLowerCase())
+
+    const matchesCompensation =
+      filterCompensation.value === '' || a.compensation === filterCompensation.value
+
+    return matchesSearch && matchesAnimal && matchesCity && matchesCompensation
+
+  })
+
+})
 async function fetchAusschreibungen(){
 try {
     const response = await fetch(url)
@@ -37,12 +63,33 @@ try {
 
   <h1 class="text-center fw-bold mb-5">
     Meine Ausschreibungen
+    <select v-model="filterAnimal" class="form-select mb-2">
+    <option value="">Alle Tiere</option>
+    <option value="DOG">Hund</option>
+    <option value="CAT">Katze</option>
+    <option value="RABBIT">Kaninchen</option>
+    <option value="BIRD">Vogel</option>
+  </select>
+
+  <input
+    v-model="filterCity"
+    class="form-control mb-2"
+    placeholder="Ort filtern"
+  />
+
+  <select v-model="filterCompensation" class="form-select mb-2">
+    <option value="">Alle Vergütungen</option>
+    <option value="Tausch">Tausch</option>
+    <option value="Bezahlung">Bezahlung</option>
+  </select>
+
+
   </h1>
 
   <div class="row g-5">
 
     <div
-      v-for="ausschreibung in ausschreibungen"
+      v-for="ausschreibung in filteredAusschreibungen"
       :key="ausschreibung.id"
       class="col-md-6"
     >
